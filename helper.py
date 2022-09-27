92db139d1da1d97e77c7b8454c8698e37bde2dda
ucc <ucc@eschbach.com>
2022-09-27 23:27:50 +0200

    added not working parse string method

--- helper.py
+++ helper.py
@@ -7,19 +7,36 @@ class RubiksHelper:
 
     def parse_moveString(self, moves):
         """ turns movestring into more compact form"""
-        move_list = moves.split()
 
-        # loop and check if doubles occur
-        for index in range(0, len(move_list), 2):
-            if index+1 >= len(move_list):
-                break
+        #TBD diese Methode funktioniert noch nicht so wie sie soll.
+        move_list = moves.split()
+        nothing_changed = True
+        index = 0
+
+        while nothing_changed:
+            nothing_changed = False
+            if index <= len(moves)-1:
+                if  ((move_list[index].isupper() and move_list[index+1].islower()) or \
+                    (move_list[index].islower() and move_list[index+1].isupper())) and \
+                    (move_list[index].casefold() == move_list[index+1].casefold()):
+                    del move_list[index+1]
+                    index = 0
+                    nothing_changed = True
+                    continue
+                elif ((move_list[index] == move_list[index+1] and "2" in move_list[index])):
+                    del move_list[index:index+2]
+                    index = 0
+                    nothing_changed = True
+                    continue
+                elif ((move_list[index] == move_list[index+1] and not "2" in move_list[index])):
+                    del move_list[index+1]
+                    move_list[index] = "2"+move_list[index].uppercase()
+                    index = 0
+                    nothing_changed = True
+                    continue
+                index += 1
             else:
-                if move_list[index] == move_list[index+1]:
-                    move_list.pop(index)
-                    move_list[index] = "2" + move_list[index].capitalize()
-                elif move_list[index] == move_list[index+1].casefold() or move_list[index].casefold() == move_list[index+1]:
-                    move_list.pop(index)
-                    move_list.pop(index)
+                index = 0
 
         move_string = " ".join(move_list)
         return move_string
@@ -52,4 +69,3 @@ class RubiksHelper:
     def get_time(self):
         # TBD: But this calculated the timedifference between start and end
         pass
-
